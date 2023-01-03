b4a2e77e69c206232c7c72b2c3ea78de50998024
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-03 14:52:51 +0100

    js_intro_console

---
+++ main.js
@@ -0,0 +1,24 @@
+
+/* helloworld = Hello World, 
+pi = 3.14, 
+burjdubai = 828m, 
+fullname = Jan Schmidt, 
+eifeltower = 324m
+camelcase = camelCase,
+kursstatus = true */
+
+let helloWorld = "Hello World"
+
+const pi = 3.14
+
+const burjDubai = "828m"
+
+let fullName = "Jan Schmidt"
+
+const eifelTower = "324m"
+
+let camelCase = "camelCase"
+
+let kursstatus = true
+
+console.log(helloWorld + " " + pi + " " + burjDubai + " " + fullName + " " + eifelTower + " " + camelCase + " " + kursstatus);
